2ee7a13fc8a9aeec2e4b5fb6215fc4a48e87ab53
felipepampalona <felipepampalona@gmail.com>
2023-08-08 14:22:30 -0300

    Subindo o Projeto do curso

---
+++ app/Http/Controllers/NoticiaController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Noticia;
+use App\Http\Requests\StoreNoticiaRequest;
+use App\Http\Requests\UpdateNoticiaRequest;
+use Illuminate\Support\Facades\Cache;
+
+class NoticiaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        /*
+        //Criar um dado dentro do BD Redis
+        Cache::put('site', 'jorgesantana.net.br', 10);
+
+        //Recuperar o dado dentro do BD Redis
+        $site = Cache::get('site');
+        echo $site;
+        */
+
+        $noticias = [];
+        
+/*
+        if(Cache::has('10_primeiras_noticias')){
+            //Recuperar o dado dentro do BD Redis
+            $noticias = Cache::get('10_primeiras_noticias');
+        }else{
+            $noticias = Noticia::orderByDesc('created_at')->limit(10)->get();
+            //Criar um dado dentro do BD Redis
+            Cache::put('10_primeiras_noticias', $noticias, 20);
+        }
+*/
+        $noticias = Cache::remember('10_primeiras_noticias', 15, function () {
+            return Noticia::orderByDesc('created_at')->limit(10)->get();
+        });
+
+        return view('noticia', ['noticias' => $noticias]);
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreNoticiaRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreNoticiaRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Noticia  $noticia
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Noticia $noticia)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Noticia  $noticia
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Noticia $noticia)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateNoticiaRequest  $request
+     * @param  \App\Models\Noticia  $noticia
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateNoticiaRequest $request, Noticia $noticia)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Noticia  $noticia
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Noticia $noticia)
+    {
+        //
+    }
+}
